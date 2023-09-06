4384f4d4472d8eb3cfd456313758860bf60d8360
pat <143344073+junl837@users.noreply.github.com>
2023-09-06 17:19:25 +0800

    added find_fav_song.sh

---
+++ myrepo/find_fav_song.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+pwd
+# this may be different depending on where you place your files
+cd ../../w02lab2-part2/inf2002-lab02-2023
+# search for the song in all the files
+grep -m1 "IU Blueming" *.txt
